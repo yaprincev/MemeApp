175f6df55b659055bcddd0f91a5893fce85da6ac
yaprincev <5489812@mail.ru>
2024-02-19 20:37:49 +0300

    init project

---
+++ MemeApp/MemeApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MemeApp
+//
+//  Created by Максим Япринцев on 19.02.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
